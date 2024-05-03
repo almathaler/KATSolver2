85af880f05f26c534b2a34d858de5e4ce0a32322
Alma Thaler <at564@cornell.edu>
2024-05-02 20:52:44 -0400

    Main with sys args

--- ka_solver/bin/main.ml
+++ ka_solver/bin/main.ml
@@ -1,1 +1,7 @@
-let () = print_endline "Hello, World!"
+let () =
+  let num_args = (Array.length Sys.argv) - 1 in 
+  if num_args = 2 then
+    (Printf.printf "First expression: %s \n" (Array.get Sys.argv 1);
+    Printf.printf "Second expression: %s \n" (Array.get Sys.argv 2))
+  else
+    Printf.printf "Expected 'dune exec bin/main.exe <exp1> <exp2>', but %i arguments were passed \n" num_args
