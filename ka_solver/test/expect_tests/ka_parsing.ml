35db7496639eda63034a8bf8a19456dc1e1b2582
Alma Thaler <at564@cornell.edu>
2024-05-03 00:50:40 -0400

    Added a few tests. For tomorrow, do ACI, brz derivatives, and check_equiv

---
+++ ka_solver/test/expect_tests/ka_parsing.ml
@@ -0,0 +1,25 @@
+open Core
+open Ka_solver
+
+let parse (s : string) : Expr.t =
+  let lexbuf = Lexing.from_string s in
+  let exp = Parser.ka_expr Lexer.read lexbuf in
+  exp
+
+let%expect_test "a+b" =
+  "a+b" |> parse |> Expr.sexp_of_t |> Core.Sexp.to_string_hum |> print_endline;
+  [%expect{| (Sum ((Prim a) (Prim b))) |}]
+
+let%expect_test "ab" =
+"ab" |> parse |> Expr.sexp_of_t |> Core.Sexp.to_string_hum |> print_endline;
+[%expect{| (Prod ((Prim a) (Prim b))) |}]
+
+let%expect_test "ab(c+d*)*" =
+"ab(c+d*)*" |> parse |> Expr.sexp_of_t |> Core.Sexp.to_string_hum |> print_endline;
+[%expect{| (Prod ((Prim a) (Prod ((Prim b) (Star (Sum ((Prim c) (Star (Prim d))))))))) |}]
+
+let%expect_test "a0(c+1*)*" =
+"a0(c+1*)*" |> parse |> Expr.sexp_of_t |> Core.Sexp.to_string_hum |> print_endline;
+[%expect{| (Prod ((Prim a) (Prod (Zero (Star (Sum ((Prim c) (Star One)))))))) |}]
+
+(* TODO: Add some ACI tests *)
---
+++ ka_solver/test/test_ka_solver.ml
@@ -0,0 +1,1 @@
+let () = print_endline "this should be a test"
