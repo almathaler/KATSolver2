41d9631f2e48165c06c040cdbdfb3094dc459622
Alma Thaler <at564@cornell.edu>
2024-05-12 17:14:23 -0400

    Sym_expr, where the tests are actually BDD

--- kat_solver/lib/bdd.ml
+++ kat_solver/lib/bdd.ml
@@ -1,4 +1,6 @@
 (** From fig. 2 of Pous. 'b is the type of the leaves and 'a is the 
     type of the decision nodes *)
-    type ('a, 'b) node = ('a, 'b) descr Hashcons.hash_consed 
-    and ('a, 'b) descr = V of 'b | N of 'a * ('a, 'b) node * ('a, 'b) node 
+type ('a, 'b) node = ('a, 'b) descr Hashcons.hash_consed 
+and ('a, 'b) descr = V of 'b | N of 'a * ('a, 'b) node * ('a, 'b) node 
+
+type ('a, 'b) t = ('a, 'b) node
--- kat_solver/lib/bdd.mli
+++ kat_solver/lib/bdd.mli
@@ -2,3 +2,5 @@
     type of the decision nodes *)
 type ('a, 'b) node = ('a, 'b) descr Hashcons.hash_consed 
 and ('a, 'b) descr = V of 'b | N of 'a * ('a, 'b) node * ('a, 'b) node 
+
+type ('a, 'b) t = ('a, 'b) node
