c33a9eb8fccc1757e9ab14f5e315375834346d74
Alma Thaler <at564@cornell.edu>
2024-05-12 23:14:15 -0400

    Untested [of_expr] for sym_kat

--- kat_solver/lib/sym_expr.ml
+++ kat_solver/lib/sym_expr.ml
@@ -1,8 +1,18 @@
+open Core 
+
 type t = 
 | Prim of char 
 | Sum of (t * t)
 | Prod of (t * t) 
 | Star of t 
 | Test of (char, bool) Bdd.t 
+[@@deriving sexp]
 
-let of_expr (_ : Expr.t) : t = failwith "todo"
+let sexp_of_t = sexp_of_t
+let rec of_expr expr = 
+  match expr with 
+  | Expr.Prim c -> Prim c 
+  | Expr.Sum (e1, e2) -> Sum (of_expr e1, of_expr e2)
+  | Expr.Prod (e1, e2) -> Prod (of_expr e1, of_expr e2)
+  | Expr.Star e -> Star (of_expr e)
+  | Expr.Test t -> Test (Test.to_bdd t)
--- kat_solver/lib/sym_expr.mli
+++ kat_solver/lib/sym_expr.mli
@@ -1,9 +1,13 @@
+open Core 
+
 type t = 
 | Prim of char 
 | Sum of (t * t)
 | Prod of (t * t) 
 | Star of t 
 | Test of (char, bool) Bdd.t 
+[@@deriving sexp] 
 
+val sexp_of_t : t -> Sexp.t
 (** Translates an expr to a sym_expr, and also normalizes while doing this. *)
 val of_expr : Expr.t -> t 
