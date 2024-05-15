bfd3421ede83689a372ddddac26ddf7f3321c215
Alma Thaler <at564@cornell.edu>
2024-05-15 01:37:28 -0400

    One test...

---
+++ kat_solver/test/expect_tests/equivalence.ml
@@ -0,0 +1,17 @@
+open Core 
+open Kat_solver 
+
+let parse (s : string) : Expr.t = 
+  let lexbuf = Lexing.from_string s in 
+  let exp = Parser.expr Lexer.read lexbuf in 
+  exp 
+
+let test str1 str2 = 
+  let sym_expr1 = str1 |> parse |> Sym_expr.of_expr in 
+  let sym_expr2 = str2 |> parse |> Sym_expr.of_expr in 
+  let (ans, _, _) = Equivalence_checker.are_equivalent sym_expr1 sym_expr2 in 
+  Printf.printf "%b" ans 
+
+let%expect_test "a, a" = 
+  test "a" "a"; 
+  [%expect {| true |}]
